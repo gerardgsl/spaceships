65f1cae79af8f42ebff469d41b87c9e069a5163a
Gerard <gerardsoley1998@gmail.com>
2024-10-29 14:21:23 +0100

    Created hangar microservice

---
+++ back/hangar/src/main/java/com/spaceship/hangar/HangarApplication.java
@@ -0,0 +1,13 @@
+package com.spaceship.hangar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangarApplication.class, args);
+	}
+
+}
